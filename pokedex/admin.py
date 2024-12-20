c2bbb0e11a8b40b76906fa7bfe977332e64eeaba
Fernando Daniel Vaca Buitrón <fdvaca@puce.edu.ec>
2024-12-20 16:46:41 -0500

    Creacion de trainer

--- pokedex/admin.py
+++ pokedex/admin.py
@@ -1,8 +1,10 @@
 from django.contrib import admin
 from .models import Pokemon
+from .models import Trainer
 
 # Register your models here.
 @admin.register(Pokemon)
+@admin.register(Trainer)
 
 class PokemonAdmin (admin.ModelAdmin):
         pass
