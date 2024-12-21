e385eec72b4c86cfd5a88f3cbce7be313ae20e97
Fernando Daniel Vaca Buitrón <fdvaca@puce.edu.ec>
2024-12-20 21:40:24 -0500

    Actualizacion trainer

--- pokedex/models.py
+++ pokedex/models.py
@@ -14,6 +14,7 @@ class Trainer(models.Model):
     first_name = models.CharField(max_length=30, null=False)
     last_name = models.CharField(max_length=30, null=False)
     level = models.IntegerField()
+    birth_date = models.DateField(null=True, blank=True)
 
     def __str__(self):
         return f"{self.first_name} {self.last_name}"
